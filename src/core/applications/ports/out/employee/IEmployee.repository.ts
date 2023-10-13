153fcdd618e48047e3b28b7833f053b773c5ac5d
Jonathan Gomes <jonathan.gomess1@gmail.com>
2023-10-13 09:46:23 -0300

    feat: cria repositorio para employee com delete, save, get, update

---
+++ src/core/applications/ports/out/employee/IEmployee.repository.ts
@@ -0,0 +1,10 @@
+import { IEmployee } from "../../../../domain/entities/IEmployee";
+
+interface IEmployeeRepository  {
+    save(employee: IEmployee): Promise<void>;
+    delete(cpf: string): Promise<void>;
+    update(employee: IEmployee): Promise<void>;
+    getAll(): Promise<IEmployee[]>
+}
+
+export { IEmployeeRepository };
