7dcf8135e95f5f602eb29611231ee3261656cab8
Fabio Marques <fabio.marquezzx@gmail.com>
2024-01-30 09:34:36 -0300

    trocando acesso db de hom para prod

--- fiap-lanches-clean/app/src/configurations/DataSource.ts
+++ fiap-lanches-clean/app/src/configurations/DataSource.ts
@@ -8,32 +8,32 @@ import { Product } from "./DataSourceModelation/ProductEntityConfig";
 import { LoggerImpl } from "./Logger/LoggerImpl";
 require('dotenv').config();
 
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "mysecretpassword",
-  database: "postgres",
-  synchronize: true,
-  logging: true,
-  entities: [Promotion, Product, Order, Client, Employee],
-  subscribers: [],
-  migrations: [],
-});
 // export const AppDataSource = new DataSource({
 //   type: "postgres",
-//   host: process.env.POSTGRES_HOST,
-//   port: 80,
-//   username: process.env.POSTGRES_USER,
-//   password: process.env.POSTGRES_PASSWORD,
-//   database: process.env.POSTGRES_DB,
+//   host: "localhost",
+//   port: 5432,
+//   username: "postgres",
+//   password: "mysecretpassword",
+//   database: "postgres",
 //   synchronize: true,
 //   logging: true,
 //   entities: [Promotion, Product, Order, Client, Employee],
 //   subscribers: [],
 //   migrations: [],
 // });
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.POSTGRES_HOST,
+  port: 80,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: true,
+  logging: true,
+  entities: [Promotion, Product, Order, Client, Employee],
+  subscribers: [],
+  migrations: [],
+});
 
 const logger = new LoggerImpl();
 AppDataSource.initialize()
